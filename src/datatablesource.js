6f9ee0eb29bc727f8db2801179a320d75b6d30ab
AdroTheWanderer <u20161269@upc.edu.pe>
2022-09-18 02:29:59 -0500

    Inicializar proyecto

---
+++ src/datatablesource.js
@@ -0,0 +1,122 @@
+export const userColumns = [
+    { field: "id", headerName: "ID", width: 70 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 230,
+      renderCell: (params) => {
+        return (
+          <div className="cellWithImg">
+            <img className="cellImg" src={params.row.img} alt="avatar" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    {
+        field:"correo", 
+        headerName:"Correo", 
+        width: 230,
+    },
+    {
+        field:"edad", 
+        headerName:"Edad", 
+        width: 100,
+    },
+    {
+        field:"status", 
+        headerName:"Status", 
+        width: 160,
+        renderCell:(params)=>{
+            return (
+                <div className={`cellWithStatus ${params.row.status}`}>
+                    {params.row.status}
+                </div>
+            )
+        }
+    },
+  ];
+
+//temporary data
+export const userRows = [
+    {
+      id: 1,
+      username: "Snow",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      status: "activo",
+      correo: "1snow@gmail.com",
+      edad: 35,
+    },
+    {
+      id: 2,
+      username: "Jamie Lannister",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "2snow@gmail.com",
+      status: "pasivo",
+      edad: 42,
+    },
+    {
+      id: 3,
+      username: "Lannister",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "3snow@gmail.com",
+      status: "pendiente",
+      edad: 45,
+    },
+    {
+      id: 4,
+      username: "Stark",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "4snow@gmail.com",
+      status: "activo",
+      edad: 16,
+    },
+    {
+      id: 5,
+      username: "Targaryen",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "5snow@gmail.com",
+      status: "pasivo",
+      edad: 22,
+    },
+    {
+      id: 6,
+      username: "Melisandre",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "6snow@gmail.com",
+      status: "activo",
+      edad: 15,
+    },
+    {
+      id: 7,
+      username: "Clifford",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "7snow@gmail.com",
+      status: "pasivo",
+      edad: 44,
+    },
+    {
+      id: 8,
+      username: "Frances",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "8snow@gmail.com",
+      status: "activo",
+      edad: 36,
+    },
+    {
+      id: 9,
+      username: "Roxie",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "snow@gmail.com",
+      status: "pendiente",
+      edad: 65,
+    },
+    {
+      id: 10,
+      username: "Roxie",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      correo: "snow@gmail.com",
+      status: "activo",
+      edad: 65,
+    },
+  ];
---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Usuario",
+      type: "text",
+      placeholder: "john_doe",
+    },
+    {
+      id: 2,
+      label: "Nombre y Apellido",
+      type: "text",
+      placeholder: "John Doe",
+    },
+    {
+      id: 3,
+      label: "Correo",
+      type: "mail",
+      placeholder: "john_doe@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Celular",
+      type: "text",
+      placeholder: "+51 999333999",
+    },
+    {
+      id: 5,
+      label: "Contraseña",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Direccion",
+      type: "text",
+      placeholder: "Av. La Marina 2134 Cdra 21 San Miguel Lima",
+    },
+    {
+      id: 7,
+      label: "País",
+      type: "text",
+      placeholder: "Peru",
+    },
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Titulo",
+      type: "text",
+      placeholder: "Zapatillas Nike Air Jordan 1 Mid",
+    },
+    {
+      id: 2,
+      label: "Descripcion",
+      type: "text",
+      placeholder: "Descripcion",
+    },
+    {
+      id: 3,
+      label: "Categoria",
+      type: "text",
+      placeholder: "Zapatillas",
+    },
+    {
+      id: 4,
+      label: "Precio",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
